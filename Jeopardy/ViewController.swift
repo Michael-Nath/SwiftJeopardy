9bc36c2b67b704dee06e6ab33bd8f0f4e7b30c5f
Michael <mnath10@stuy.edu>
2020-07-07 12:49:51 -0400

    Added questions/answers for the science category

--- Jeopardy/ViewController.swift
+++ Jeopardy/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        showCategories()
     }
 
-
+    @IBOutlet var categories: [UILabel]!
+    let categoryOneQuestions = [
+    "How many electrons are there in a Helium atom?",
+    "It can't fly or breathe fire but this largest living lizard has a venomous bite that inhibits blood clotting",
+    "It's the largest blood vessel in the body",
+    "Of DNA's 8 bases this one was first discovered in animal dung hence its name",
+    "Seems counterintuitive but the Earth is farthest from the sun during this month",
+    ]
+    let categoryOneAnswers = [
+        "2",
+        "Komodo Dragon",
+        "Aorta",
+        "Guanine",
+        "July"
+    ]
+    func showCategories() {
+        for category in categories {
+            print(category.text!)
+        }
+    }
 }
 
