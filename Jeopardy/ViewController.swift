af8e284eec78fbff72b1de5e192c095017262b15
Michael <mnath10@stuy.edu>
2020-07-09 10:55:13 -0400

    points are being displayed correctly; next step: simulate a full round

--- Jeopardy/ViewController.swift
+++ Jeopardy/ViewController.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var currentPoints = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pointsDisplay.text = "Points : \(currentPoints)"
     }
 
     @IBOutlet var pointsDisplay: UILabel!
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var categories: [UILabel]!
+//    Store all the questions and answers (2D Array in future)
     let categoryOneQuestions = [
     "How many electrons are there in a Helium atom?",
     "It can't fly or breathe fire but this largest living lizard has a venomous bite that inhibits blood clotting",
@@ -30,11 +32,15 @@ class ViewController: UIViewController {
         "Komodo Dragon",
         "Aorta",
         "Guanine",
-        "July "
+        "July"
     ]
     //    do the same for every other category/answer
     var currentQuestion : JQuestion!
+    var currentButton: UIButton!
+    
+//    handles the action of choosing a question
     @IBAction func questionPressed(_ sender: UIButton) {
+        currentButton = sender
         currentQuestion = JQuestion(
             question: categoryOneQuestions[sender.tag],
             answer: categoryOneAnswers[sender.tag],
@@ -43,6 +49,8 @@ class ViewController: UIViewController {
         showQuestion()
 
     }
+    
+//    gives a popup in which an user can answer the displayed question
     func showQuestion() {
         questionLabel.text = currentQuestion.question
         let alert = UIAlertController(title: "Answer", message: currentQuestion.question, preferredStyle: .alert)
@@ -53,13 +61,24 @@ class ViewController: UIViewController {
         self.present(alert, animated:true, completion: nil)
     }
     
+// verifies the proposed answer to the question
     func verifyAnswer(_ answer: String?) {
         if answer == currentQuestion.answer {
             questionLabel.text = "Correct!"
+            currentPoints += currentQuestion.points
         } else {
             questionLabel.text = "Incorrect!"
         }
+        updateUI()
     }
+//    updates points (later will create new gameRound)
+    func updateUI() {
+        currentButton.isEnabled = false
+//      alpha is like the opacity of the button
+        currentButton.alpha = 0.5
+        pointsDisplay.text = "Points : \(currentPoints)"
+    }
+//   makes the dollar-signed points easier to work with
     func formattedPoints(_ text : String) -> Int {
         var myStr = ""
         for char in text {
